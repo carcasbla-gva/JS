dc1fbf973079529809d1cc04257cf9ed8496c10a
carcasbla-gva <carcasbla@alu.edu.gva.es>
2026-03-27 08:20:05 +0100

    Ejercicios

--- Ejercicio Final/app.js
+++ Ejercicio Final/app.js
@@ -7,13 +7,10 @@ class Tarea {
 
 class GestorTareas {
     constructor() {
-        // Seleccionamos los elementos del DOM (Punto 5)
         this.input = document.querySelector('#nueva-tarea-input');
         this.boton = document.querySelector('#agregar-tarea-btn');
         this.lista = document.querySelector('#lista-tareas');
-        
         this.tareas = [];
-
         this.iniciarEventos();
     }
 
@@ -25,20 +22,15 @@ class GestorTareas {
 
     agregarNuevaTarea() {
         const texto = this.input.value;
-
         if (texto !== "") {
             const nuevaTarea = new Tarea(texto);
             this.tareas.push(nuevaTarea);
-
             const li = document.createElement('li');
             li.textContent = nuevaTarea.texto;
-
             li.addEventListener('click', (event) => {
                 event.target.classList.toggle('completada')
             });
-
             this.lista.appendChild(li);
-
             this.input.value = "";
         }
     }
